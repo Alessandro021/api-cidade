66f5d08af267ea61a8f136e426b24bc629120ab0
alessandro <alessandrodamata6@gmail.com>
2023-06-10 21:29:26 -0300

    Iniciando testes no controller cidades

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import * as yup from "yup";
-import { validation } from "../../shared/middleware";
 import { StatusCodes } from "http-status-codes";
 
+import { validation } from "../../shared/middleware";
 
 interface Icidade {
   nome: string
@@ -11,28 +11,14 @@ const bodyValidation : yup.ObjectSchema<Icidade> = yup.object().shape({
   nome: yup.string().required().min(3),
 });
 
-
-// interface IFilter {
-//   filter?: string;
-// }
-// const queryValidation : yup.ObjectSchema<IFilter> = yup.object().shape({
-//   filter: yup.string().optional().min(3),
-// });
-
-
 export const createValidation = validation({
   body: bodyValidation,
 });
 
 
-
-
 export const create = async (req: Request<{}, {}, Icidade> , res: Response) =>  {
   // const data: Icidade = req.body;
-
-
   console.log(req.body);
 
-
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não Implementado");
+  return res.status(StatusCodes.CREATED).json(1);
 };
---
+++ tests/cidades/Create.test.ts
@@ -0,0 +1,36 @@
+import { StatusCodes } from "http-status-codes";
+import { testServer } from "../jest.setup";
+
+describe("Cidades - Create", () => {
+
+
+  it("Cria registro", async () => {
+    const res1 = await testServer.post("/cidades").send({
+      nome: "Urucuia"
+    });
+
+
+    expect(res1.statusCode).toEqual(StatusCodes.CREATED);
+    expect(typeof res1.body).toEqual("number");
+  });
+
+  it("Tentar criar registro com nome muito curto", async () => {
+    const res1 = await testServer.post("/cidades").send({
+      nome: "Ur"
+    });
+
+
+    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(res1.body).toHaveProperty("errors.body.nome");
+  });
+
+  it("requisição sem campo obrigatorio", async () => {
+    const res1 = await testServer.post("/cidades").send({
+      
+    });
+
+
+    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(res1.body).toHaveProperty("errors.body.nome");
+  });
+});
